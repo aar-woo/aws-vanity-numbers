90f0375a1bc67ac3c341c3ae9a5fe18a5c24377e
Aaron Woo <awoo@chapman.edu>
2021-12-28 14:37:51 -1000

    Created a handler function that successfully returns the phone number from an example connect request event.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ const AWS = require("aws-sdk");
 const convertNumber = require('./convert-number');
 const insertVanityNumbers = require('./insert-vanity-numbers');
 
-insertVanityNumbers('463-3339');
+exports.handler = async (event, context, callback) => {
+  const phoneNum = event['Details']['ContactData']['CustomerEndpoint']['Address'];
+  const response = {
+    PhoneNumber: phoneNum
+  }
+  callback(null, response);
+}
+
+// insertVanityNumbers('463-3339');
